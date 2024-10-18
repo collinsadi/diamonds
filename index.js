1acbdc2d4a11d7303fbbbafb83b40abad232b5fc
collinsadi <collinsadi20@gmail.com>
2024-10-18 17:20:34 +0100

    fix: fixed exports from createScaffold.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 const promptUser = require("./utils/promptUser");
-const createScaffold = require("./utils/createScaffold");
+const processOptions = require("./utils/createScaffold");
 
 const run = async () => {
   const options = await promptUser();
-  await createScaffold(options);
+  await processOptions(options);
 };
 
 run();
