ebe621a3d883578703b3ec01ff5804130a75f8ef
obah <olusojiobah@gmail.com>
2024-11-26 08:39:43 +0100

    feat: initialised default hardhat

---
+++ src/helpers/scaffoldERC721/scaffoldERC721Foundry.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const colors = require("colors");
+const copyTemplateFiles = require("../../utils/file-manager");
+const templateFolder = path.join(
+  __dirname,
+  "..",
+  "..",
+  "templates",
+  "ERC20",
+  "foundry"
+);
+
+const scaffoldERC20Foundry = async (name) => {
+  await copyTemplateFiles(templateFolder, name);
+};
+
+module.exports = scaffoldERC20Foundry;
